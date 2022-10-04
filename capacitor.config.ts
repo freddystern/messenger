7c4a7fafc67621c817496ce72294cce26c843e94
fdr <f.felix.fm@ndr.de>
2022-10-04 10:22:26 +0200

    added capacitor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'de.ndr.app.ndrhh',
   appName: 'messi',
   webDir: 'www',
   bundledWebRuntime: false
